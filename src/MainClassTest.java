4487539eac3220ae2117b17bd356f34f3e4fcbbc
Andrey Kravz <andrey.bigalinov@gmail.com>
2021-04-13 00:39:57 +0300

    Test 3

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -2,12 +2,13 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClassTest extends MainClass {
-    
+
+    String a = getClassString();
+
     @Test
-    public void testGetClassNumber() {
-        int b = 45;
-        int a = getClassNumber();
-        Assert.assertTrue("a <= 45", a > b);
-        System.out.println("Test OK");
+    public void testGetClassString() {
+        Assert.assertTrue("Doesn't contain a string", a.contains("Hello") || a.contains("hello"));
+        System.out.println("Line is contained");
     }
+
 }
